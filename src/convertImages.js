24b41959c5fe74e4655ecd83c72095ce98436182
Tor Nordqvist <tornord@gmail.com>
2022-10-03 22:53:30 +0200

    added image server

---
+++ src/convertImages.js
@@ -0,0 +1,45 @@
+const { join, resolve, extname } = require("path");
+const fs = require("fs");
+
+const Jimp = require("jimp");
+
+function getDates(imagePath) {
+  const files = fs.readdirSync(join(imagePath));
+  const res = [];
+  for (const f of files) {
+    if (!/\d{4}-\d{2}-\d{2}/.test(f)) continue;
+    res.push(f);
+  }
+  return res;
+}
+
+async function main() {
+  const screenshotsPath = resolve(".", "screenshots");
+  const thumbnailsPath = resolve(".", "thumbnails");
+  const dates = getDates(screenshotsPath);
+  const folders = ["done", "anomalies"];
+  for (const date of dates) {
+    for (const folder of folders) {
+      const srcPath = join(screenshotsPath, date, folder);
+      if (!fs.existsSync(srcPath)) continue;
+      const destPath = join(thumbnailsPath, date, folder);
+      const files = fs.readdirSync(srcPath);
+      for (const f of files) {
+        const filename = join(srcPath, f);
+        // const s = fs.statSync(filename);
+        const ext = extname(f);
+        if (!/\.(jpe?g|png)$/.test(ext)) continue;
+        const outFilename = join(destPath, f.replace(/\.(jpe?g|png)$/, ".jpg"));
+        if (fs.existsSync(outFilename)) continue;
+        const img = await Jimp.read(filename);
+        // console.log(f, img.bitmap);
+        await img.resize(80, 120);
+        await img.quality(80);
+        await img.write(outFilename);
+      }
+      console.log(date, folder);
+    }
+  }
+}
+
+main();
---
+++ src/imageServer.js
@@ -0,0 +1,66 @@
+const { join, resolve, extname } = require("path");
+const fs = require("fs");
+
+const express = require("express");
+const cors = require("cors");
+
+const dateToString = (d) => d.toISOString().slice(0, 10);
+const today = () => dateToString(new Date());
+const MS_PER_DAY = 24 * 3600 * 1000;
+
+const IMAGE_PATH = resolve(".", "screenshots");
+// const IMAGE_PATH = resolve(".", "thumbnails");
+
+function getImages(name, type, date) {
+  const files = fs.readdirSync(join(IMAGE_PATH));
+  let res = [];
+  for (const f of files) {
+    if (!/\d{4}-\d{2}-\d{2}/.test(f)) continue;
+    getImagesByFolder(res, f, "done");
+    getImagesByFolder(res, f, "anomalies");
+  }
+  if (name || type || date) {
+    res = res.filter((d) => (!name || d.name === name) && (!type || d.type === type) && (!date || d.date === date));
+  }
+  res.sort((d1, d2) => d1.changeTimestamp - d2.changeTimestamp);
+  return res;
+}
+
+function getImagesByFolder(res, date, folder) {
+  const path = join(IMAGE_PATH, date, folder);
+  if (!fs.existsSync(path)) return;
+  const files = fs.readdirSync(path);
+  const cutOff = Date.now() - MS_PER_DAY;
+  for (const f of files) {
+    const s = fs.statSync(join(path, f));
+    const changeTimestamp = Math.floor(s.ctimeMs);
+    // if (changeTimestamp < cutOff) continue;
+    const ext = extname(f);
+    if (!/\.(je?pg|png)/.test(ext)) continue;
+    const m = f.match(/^([\.a-z0-9-]+)(_\d+)?\.(je?pg|png)$/);
+    // if (!m) continue;
+    if (!m) {
+      console.log("match");
+    }
+    const name = m[1];
+    const index = m[2] ? m[2].slice(1) : null;
+    if (index !== null) {
+      console.log("index");
+    }
+    res.push({ url: `./images/${date}/${folder}/${f}`, changeTimestamp, date, type: folder, name, index });
+  }
+}
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  const name = req.query.name ?? null;
+  const type = req.query.type ?? null;
+  const date = req.query.date ?? null;
+  res.send(getImages(name, type, date));
+});
+
+app.use("/images", express.static(IMAGE_PATH));
+
+app.listen(3000);
